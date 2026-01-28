1a379a2cac815bed5efd5f134f015f4a032ec616
UjjwalFullStackDev <ujjwal.tyagi90611@gmail.com>
2026-01-28 13:23:18 +0530

    Fixed rendering issue

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -1,25 +1,51 @@
-import { useGLTF } from '@react-three/drei'
 import { useRef } from 'react'
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 
 const Target = (props) => {
-    const targetRef = useRef();
-    const {scene} = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf')
+  const targetRef = useRef();
 
-    useGSAP(() => {
-        gsap.to(targetRef.current.position, {
-            y: targetRef.current.position.y + 0.5,
-            duration: 1.5,
-            repeat: -1,
-            yoyo: true
-        })
-    },[])
+  useGSAP(() => {
+    gsap.to(targetRef.current.position, {
+      y: targetRef.current.position.y + 0.5,
+      duration: 1.5,
+      repeat: -1,
+      yoyo: true
+    })
+  }, [])
 
   return (
-    <mesh {...props} ref={targetRef} rotation={[0, Math.PI / 5, 0]} scale={1.5}>
-        <primitive object={scene} />
-    </mesh>
+    <group {...props} ref={targetRef} rotation={[0, Math.PI / 5, 0]} scale={1.5}>
+      {/* Base stand */}
+      <mesh position={[0, -1.5, 0]}>
+        <cylinderGeometry args={[0.15, 0.15, 3, 32]} />
+        <meshStandardMaterial color="#8b4513" metalness={0.3} roughness={0.7} />
+      </mesh>
+
+      {/* Target board backing */}
+      <mesh position={[0, 0, 0]}>
+        <cylinderGeometry args={[1, 1, 0.1, 32]} />
+        <meshStandardMaterial color="#f5f5dc" metalness={0.1} roughness={0.8} />
+      </mesh>
+
+      {/* Target rings - red */}
+      <mesh position={[0, 0, 0.06]} rotation={[Math.PI / 2, 0, 0]}>
+        <torusGeometry args={[0.8, 0.15, 16, 32]} />
+        <meshStandardMaterial color="#dc143c" metalness={0.2} roughness={0.6} />
+      </mesh>
+
+      {/* Target rings - white */}
+      <mesh position={[0, 0, 0.07]} rotation={[Math.PI / 2, 0, 0]}>
+        <torusGeometry args={[0.5, 0.15, 16, 32]} />
+        <meshStandardMaterial color="#ffffff" metalness={0.2} roughness={0.6} />
+      </mesh>
+
+      {/* Target center - red */}
+      <mesh position={[0, 0, 0.08]}>
+        <cylinderGeometry args={[0.2, 0.2, 0.05, 32]} />
+        <meshStandardMaterial color="#dc143c" metalness={0.2} roughness={0.6} />
+      </mesh>
+    </group>
   )
 }
 
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -11,14 +11,15 @@ const About = () => {
 
         setTimeout(() => {
             setHasCoied(false);
-    }, 2000)};
+        }, 2000)
+    };
 
     return (
         <section className='c-space my-20' id='about'>
             <div className='grid xl:grid-cols-3 xl:grid-row-6 md:grid-cols-2 grid-cols-1 gap-5 h-full'>
                 <div className='col-span-1 xl:row-span-3'>
                     <div className='grid-container'>
-                        <img src="/assets/grid1.png" alt="grid-1" className='w-full sm:h-[276] h-fit object-contain ' />
+                        <img src="/assets/grid1.png" alt="grid-1" className='w-full sm:h-[276] h-fit object-contain ' draggable={false} />
                         <div>
                             <p className='grid-headtext'>Hay, I am Ujjwal</p>
                             <p className='grid-subtext'>With 2 years of experience, I have honed my skills in frontend and backend development, with a focus on 3D animation websites. </p>
@@ -27,7 +28,7 @@ const About = () => {
                 </div>
                 <div className='col-span-1 xl:row-span-3'>
                     <div className='grid-container'>
-                        <img src="/assets/grid2.png" alt="grid-2" className='w-full sm:h-[276px] h-fit object-contain ' />
+                        <img src="/assets/grid2.png" alt="grid-2" className='w-full sm:h-[276px] h-fit object-contain ' draggable={false} />
                         <div>
                             <p className='grid-headtext'>Tech Stack</p>
                             <p className='grid-subtext'>I specialized in JavaScript / Tailwind / BootStrap with a focus on React, Node and Next.js ecosystem</p>
@@ -52,14 +53,14 @@ const About = () => {
                             <p className='grid-headtext'>I work remotely across most timezomes</p>
                             <p className='grid-subtext'>I'm based in India, with remote work available</p>
                             <a href="#contact" className="w-fit">
-                            <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+                                <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
                             </a>
                         </div>
                     </div>
                 </div>
                 <div className='xl:col-span-2 xl:row-span-3'>
                     <div className='grid-container'>
-                        <img src="/assets/grid3.png" alt="grid-3" className='w-full sm:h-[266px] h-fit object-contain' />
+                        <img src="/assets/grid3.png" alt="grid-3" className='w-full sm:h-[266px] h-fit object-contain' draggable={false} />
                         <div>
                             <p className='grid-headtext'>My Passion for Coding</p>
                             <p className='grid-subtext'>I love solve problems and building things through codes. Coding isn't just my proffession - it is my passion.</p>
@@ -68,7 +69,9 @@ const About = () => {
                 </div>
                 <div className='xl:col-span-1 xl:row-span-2'>
                     <div className='grid-container'>
-                        <img src="/assets/grid4.png" alt="grid-4" className='w-full sm:h-[126px]sm:h-[276px] h-fit object-contain sm:object-top'/>
+                        <img src="/assets/grid4.png" alt="grid-4" className='w-full sm:h-[126px]sm:h-[276px] h-fit object-contain sm:object-top'
+                            draggable={false}
+                        />
                         <div className='space-y-2'>
                             <p className='grid-headtext text-center'>
                                 Contact Me
